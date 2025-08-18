c9c88beb88ee9e4a09f165d980ce09d84c18ac92
校奥浆糊 <wst22@mails.tsinghua.edu.cn>
2025-08-18 13:35:36 +0000

    fix: dependencies and install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+conda create -n CIDNet python=3.9.0
+conda activate CIDNet
+conda install pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda=11.7 -c pytorch -c nvidia
+pip install -r requirements.txt
